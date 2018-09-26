b0c7a9eb438db90e4f46ed1144d464491947d1a5
BuffaloRob <peckham.rob@gmail.com>
2018-09-26 10:41:27 -0400

    name input is now connected and updates in state automatically

--- src/03-basic-input.js
+++ src/03-basic-input.js
@@ -6,15 +6,20 @@ document.body.appendChild(content);
 module.exports = class extends React.Component {
   static displayName = "03-basic-input";
 
-  state = { names: [] };
+  state = { 
+    name: '',
+    names: [],
+  };
 
   onFormSubmit = (evt) => {
-    const name = this.refs.name.value;
-    const names = [...this.state.names, name ];
-    this.setState({ names: names });
-    this.refs.name.value = '';
+    const names = [...this.state.names, this.state.name ];
+    this.setState({ names: names, name: '' });
     evt.preventDefault();
   };
+
+  onNameChange = (evt) => {
+    this.setState({ name: evt.target.value });
+  }
 
   render() {
     return (
@@ -24,7 +29,8 @@ module.exports = class extends React.Component {
         <form onSubmit={this.onFormSubmit}>
           <input
             placeholder='Name'
-            ref='name'
+            value={this.state.name}
+            onChange={this.onNameChange}
           />
 
           <input type='submit' />
